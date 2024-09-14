2c6fc66d8c5912af3ada4639ecf952e9c36a732a
Mateus Alves <153127373+suco-mateusalves@users.noreply.github.com>
2024-09-13 21:44:45 -0300

    Add files via upload

--- aula3.py
+++ aula3.py
@@ -38,3 +38,53 @@ SNS.boxplot([variavel1, variavel2])
 PLT.xticks([0, 1], ['SepalLengthCm', 'SepalWidthCm'])
 PLT.show()
 
+#Species
+irissetosa = Iris[Iris['Species']=='Iris-setosa'] 
+irisversicolor = Iris[Iris['Species']=='Iris-versicolor']
+irisvirginica = Iris[Iris['Species']=='Iris-virginica']
+'''''
+#Iris Setosa
+SNS.scatterplot(data=irissetosa, x='SepalLengthCm', y='SepalWidthCm')
+PLT.title("Setosa: SepalLengthCm vs SepalWidthCm")
+PLT.show()
+SNS.scatterplot(data=irissetosa, x='SepalLengthCm', y='PetalLengthCm')
+PLT.title("Setosa: SepalLengthCm vs PetalLengthCm")
+PLT.show()
+SNS.scatterplot(data=irissetosa, x='PetalLengthCm', y='PetalWidthCm')
+PLT.title("Setosa: PetalLengthCm vs PetalWidthCm")
+PLT.show()
+SNS.scatterplot(data=irissetosa, x='SepalWidthCm', y='PetalWidthCm')
+PLT.title("Setosa: SepalWidthCm vs PetalWidthCm")
+PLT.show()
+
+#Iris Versicolor
+SNS.scatterplot(data=irisversicolor, x='SepalLengthCm', y='SepalWidthCm')
+PLT.title("SepalLengthCm vs SepalWidthCm")
+PLT.show()
+SNS.scatterplot(data=irisversicolor, x='SepalLengthCm', y='PetalLengthCm')
+PLT.title("SepalLengthCm vs PetalLengthCm")
+PLT.show()
+SNS.scatterplot(data=irisversicolor, x='PetalLengthCm', y='PetalWidthCm')
+PLT.title("PetalLengthCm vs PetalWidthCm")
+PLT.show()
+SNS.scatterplot(data=irisversicolor, x='SepalWidthCm', y='PetalWidthCm')
+PLT.title("SepalWidthCm vs PetalWidthCm")
+PLT.show()
+
+#Iris Virginica
+SNS.scatterplot(data=irisvirginica, x='SepalLengthCm', y='SepalWidthCm')
+PLT.show()
+SNS.scatterplot(data=irisvirginica, x='SepalLengthCm', y='PetalLengthCm')
+PLT.show()
+SNS.scatterplot(data=irisvirginica, x='PetalLengthCm', y='PetalWidthCm')
+PLT.show()
+SNS.scatterplot(data=irisvirginica, x='SepalWidthCm', y='PetalWidthCm')
+PLT.show()
+'''''
+irissetosa_drop = irissetosa.drop(columns=['Id','Species'])
+#print(irissetosa_drop.head())
+
+corrmatrix_irissetosa_drop = irissetosa_drop.corr()
+print(corrmatrix_irissetosa_drop)
+SNS.heatmap(corrmatrix_irissetosa_drop, annot=True)
+PLT.show()
