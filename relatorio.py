3d63070752e146fa4b62862be25e5473dc54c552
Mateus Alves <153127373+suco-mateusalves@users.noreply.github.com>
2024-09-20 21:27:53 -0300

    Add files via upload

---
+++ relatorio.py
@@ -0,0 +1,13 @@
+import pandas as PD
+import csv
+import seaborn as SNS
+import numpy as NP
+import statistics as sts
+import matplotlib.pyplot as PLT
+import matplotlib.dates as mdates
+import scipy.stats
+import scipy
+
+Iris = PD.read_csv('Iris.csv')
+Stock = PD.read_csv('stock_data.csv')
+
